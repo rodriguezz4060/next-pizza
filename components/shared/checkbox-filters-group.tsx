be70fb921fe77b347cb149c782d43b3f5243c6dd
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-07-24 16:19:50 +0300

    Next Pizza
    start of a new course on next js
    
    stack:
    1. NextJS (Parallel Routes, Group Routes, Server Actions, API)
    2. TypeScript
    3. TailwindCSS + ShadCN
    4. Prisma + PostgreSQL
    5. NextAuth
    6. React Hook Form + Zod
    7. Zustand
    8. react-use
    9. nextjs-toploader
    10. react-hot-toast
    11. react-insta-stories
    12. lucide-react
    13. Resend

---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React from "react";
+import { FilterChecboxProps, FilterCheckbox } from "./filter-checkbox";
+import { Input } from "../ui";
+
+type Item = FilterChecboxProps;
+
+interface Props {
+  title: string;
+  items: Item[];
+  defaultItems: Item[];
+  limit?: number;
+  searchInputPlacejolder?: string;
+  onChange?: (values: string[]) => void;
+  defaultValue?: string[];
+  className?: string;
+}
+
+export const CheckboxFiltersGroup: React.FC<Props> = ({
+  title,
+  items,
+  defaultItems,
+  limit = 5,
+  searchInputPlacejolder = "Поиск...",
+  onChange,
+  defaultValue,
+  className,
+}) => {
+  const [showAll, setShowAll] = React.useState(false);
+
+  const list = showAll ? items : defaultItems?.slice(0, limit);
+
+  return (
+    <div className={className}>
+      <p className="font-bold mb-3">{title}</p>
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            placeholder={searchInputPlacejolder}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
+      <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+        {list.map((item, index) => (
+          <FilterCheckbox
+            onCheckedChange={(ids) => console.log(ids)}
+            checked={false}
+            key={String(item.value)}
+            value={item.value}
+            text={item.text}
+            endAdornment={item.endAdornment}
+          />
+        ))}
+      </div>
+
+      {items.length > limit && (
+        <div className={showAll ? "border-t border-t-neutral-100 mt-4" : ""}>
+          <button
+            onClick={() => setShowAll(!showAll)}
+            className="text-primary mt-3"
+          >
+            {showAll ? "Скрыть" : "+ Показать все"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
