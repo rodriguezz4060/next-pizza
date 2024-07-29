624c04d8f5a384827f0c381d7db7d1ed502079ce
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-07-29 16:28:02 +0300

    add search

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from "./container";
 import Image from "next/image";
 import { Button } from "../ui";
 import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 interface Props {
   className?: string;
@@ -15,15 +17,23 @@ export const Header: React.FC<Props> = ({ className }) => {
       <div>
         <Container className="flex item-center justify-between py-8">
           {/** Левая часть */}
-          <div className="flex items-center gap-4">
-            <Image src="/logo.png" alt="Logo" width={35} height={35} />
-            <div>
-              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-              <p className="text-sm text-gray-400 leading-3">
-                Вскуснее уже некуда
-              </p>
+          <Link href="/">
+            <div className="flex items-center gap-4">
+              <Image src="/logo.png" alt="Logo" width={35} height={35} />
+              <div>
+                <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                <p className="text-sm text-gray-400 leading-3">
+                  Вскуснее уже некуда
+                </p>
+              </div>
             </div>
+          </Link>
+
+          {/** Центральная часть */}
+          <div className="mx-10 flex-1">
+            <SearchInput />
           </div>
+
           {/** Правая часть */}
           <div className="flex items-center gap-3">
             <Button variant="outline" className="flex items-center gap-1">
---
+++ services/constans.ts
@@ -0,0 +1,3 @@
+export enum ApiRoutes {
+  SEARCH_PRODUCT = "/products/search",
+}
